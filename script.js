a9dce03116e3828cd9dee13883a35bade95c2583
Denzel Sapatin <dknsapatin@gmail.com>
2022-02-15 15:21:55 -0500

    Added a Leaflet library on the right side of the section | Set coordinates according to the Golocation lang and long.

--- script.js
+++ script.js
@@ -3,10 +3,42 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
+const form = document.querySelector('.form');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // Set the position coordinates of both Latitude and longitude
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
+
+      //Set our GEOLocation latitude and longitude to const Coords
+      const coords = [latitude, longitude];
+
+      //   LEAFLET JS LIBRARY
+
+      //   Set const coords inside the setView
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      //   Set const coords inside the marker
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
